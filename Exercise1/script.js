39e7dba002365d212f04ab7e535bd6d39da80ffa
pottor02 <doramon_toon_5@hotmail.com>
2023-03-10 13:14:16 +0700

    commit01

--- Exercise1/script.js
+++ Exercise1/script.js
@@ -1,10 +1,17 @@
-function myFunction() {
-    let text;
-    let person = prompt("Please enter your name:", "Harry Potter");
-    if (person == null || person == "") {
-      text = "User cancelled the prompt.";
-    } else {
-      text = "Hello " + person + "! How are you today?";
-    }
-    document.getElementById("name").innerHTML = text;
+function ChangeName01() {
+  let text;
+  let person = prompt("Please enter your name:", "Insert name here!!!!!!!!!!!!!!!!");
+  if (person == null || person == "") {
+    text = "User cancelled the prompt.";
+  } else {
+    text = "Hello " + person + "! How are you today?";
   }
+  document.getElementById("name").innerHTML = text;
+}
+
+
+function ChangeName02() {
+  let text;
+  let person = prompt("Please enter your name:", "Insert name here!!!!!!!!!!!!!!!!");
+  document.getElementById("name02").innerHTML = text;
+}
